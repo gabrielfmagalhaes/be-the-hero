a0e90b539f9d97fc2428da9a740d6d637d65217a
Gabriel <magalhaes.gabrielf@outlook.com>
2020-03-29 15:59:52 -0300

    Adicoes de testes de rotas e Jest

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-
-const routes = require('./routes');
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-
+const app = require('./app');
 
 app.listen(3333);
